856853d6a944d2ea539c52fdeb895017abec9b43
bruno-a472 <brunoangelo2015@hotmail.com>
2025-04-25 21:10:00 -0300

    login att
    Login ajustado com sucesso, agora o usuário digita email+senha e o código enviado por SMS para fazer login

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,9 +1,8 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, request
+from flask_cors import CORS # type: ignore
 
 import uuid
 import json
-from flask import jsonify
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -12,6 +11,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 import arvoreDriver
 import gera_id
 
@@ -21,34 +21,74 @@ ids = gera_id.GeraId()
 # Instanciar o primeiro nó
 arvore = arvoreDriver.ArvDriver(id = ids.geraId())
 
-def login(u: str, s: str, id: int):
+def login(e: str, s: str, id: int):
+    driver: webdriver.Chrome = arvore.encontra(id).obtemDriver() # driver, do tipo webdriver.Chrome, recebe...
+    wait = WebDriverWait(driver, 20)
+    try:    
+        # Espera o botão/formulário e clica
+        botao = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "uc_flex-r")))
+        botao.click()
+        print('Botão clicado')
+
+        # Espera o campo de e-mail
+        campo_email = wait.until(EC.presence_of_element_located((By.NAME, "loginfmt")))
+        campo_email.send_keys(e)
+        campo_email.send_keys(Keys.ENTER)
+
+        # Espera o campo de senha
+        campo_senha = wait.until(EC.presence_of_element_located((By.NAME, "passwd")))
+        print('Digitando senha...')
+        campo_senha.send_keys(s)
+        botao_entrar = wait.until(EC.element_to_be_clickable((By.ID, "idSIButton9")))
+        botao_entrar.click()
+
+        # Espera o botão de confirmação (div do perfil)
+        campo_texto = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "table-row")))
+        nome = campo_texto.find_element(By.TAG_NAME, 'div')
+        # nome = nome.find_element(By.TAG_NAME, 'div').text
+        print(f'Campo encontrado: {nome}')
+        campo_texto.click()
+
+        print("Aguardando autenticação por dois fatores (digite o código)...")
+     
+
+    except TimeoutException as e:
+        print("Erro de tempo ao carregar um elemento da página:", str(e))
+        return {'bool': False}
+    
+    resposta = {'bool': True, 'id': id}
+    resposta = json.dumps(resposta, ensure_ascii=False, indent=4)
+    print("Login realizado com sucesso!")
+    return resposta
+
+def confirmacao(c: str, id: int):
     driver: webdriver.Chrome = arvore.encontra(id).obtemDriver()
+    wait = WebDriverWait(driver, 8)
+    resposta = {'bool': False} # Padrão da resposta
+
+
     try:
-        WebDriverWait(driver, 2).until(
-            EC.presence_of_all_elements_located((By.ID, "vSIS_USUARIOID"))
-        )
-        driver.find_element(By.ID, "vSIS_USUARIOID").send_keys(u)
-        driver.find_element(By.ID, "vSIS_USUARIOSENHA").send_keys(s)
-        driver.find_element(By.NAME, "BTCONFIRMA").click()
+        campo_codigo = wait.until(EC.presence_of_element_located((By.ID, "idTxtBx_SAOTCC_OTC")))
+        print('Digitando código...')
+        campo_codigo.send_keys(c)
+        campo_codigo.send_keys(Keys.ENTER)
+
         try:
-            WebDriverWait(driver, 4).until(
-                EC.presence_of_all_elements_located((By.ID, "span_MPW0041vPRO_PESSOALNOME"))
-            )
-            nome = driver.find_element(By.ID, 'span_MPW0041vPRO_PESSOALNOME').text
-            nome = nome[0:-2]
-            nome = nome.title().split()[0]
-
-            resposta = {'bool': True, 'nome': nome, 'id': id}
-            resposta = json.dumps(resposta, ensure_ascii=False, indent=4)
-
-            # print('Sucesso no login, retornando True')  # Adicionar em caso de testes
-            return resposta
-        except:
-            # print("Falha no login, retornando False")  # Adicionar em caso de testes
-            return {'bool': False}
-    except:
-        print("Login não encontrado")
-
+            span = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "uc_appuser-name")))
+            nome = 'vazio'
+            nome = str(span.text.strip().lower()).split(' ')[1] 
+            print(nome)
+            if nome != 'vazio':
+                resposta = {'bool': True, 'nome': nome} # Resposta sucesso
+                resposta = json.dumps(resposta, ensure_ascii=False, indent=4)
+            else:
+                print("Não foi encontrado o nome:", nome)
+        except Exception as e:
+            print(e)
+    except Exception as e:
+        print("Um problema foi encontrado:", e)
+    print('Terminando requisição confirmação. ID:', id)
+    return resposta
 
 def notas_parciais(id: int):
     driver: webdriver.Chrome = arvore.encontra(id).obtemDriver()
@@ -132,15 +172,15 @@ CORS(app)
 def receber_login():
     print('\n\n\n\ndados recebidos para login')
     dados = request.json 
-    u = dados['usuario']
+    e = dados['email']
     s = dados['senha']
     # print(dados)  # Adicionar em caso de testes
     chrome_options = Options()
-    # chrome_options.add_experimental_option('detach', True)
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")  
-    chrome_options.add_argument("--no-sandbox")  
-    chrome_options.add_argument("--disable-dev-shm-usage")  
+    chrome_options.add_experimental_option('detach', True)
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--disable-gpu")  
+    # chrome_options.add_argument("--no-sandbox")  
+    # chrome_options.add_argument("--disable-dev-shm-usage")  
 
 
     id = ids.geraId()
@@ -148,12 +188,22 @@ def receber_login():
     arvore.insere(id = id, driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), 
                                                 options=chrome_options))
     # print('inserido uma nova árvore') # Adicionar em caso de testes
-    arvore.encontra(id).obtemDriver().get('https://siga.cps.sp.gov.br/aluno/login.aspx?')
+    arvore.encontra(id).obtemDriver().get('https://siga.cps.sp.gov.br/sigaaluno/applogin.aspx')
 
     # print('Árvore') # Adicionar em caso de testes
     # arvore.mostra_EmOrdem() # Adicionar em caso de testes
     # print('Return') # Adicionar em caso de testes
-    return login(u, s, id)
+    return login(e, s, id)
+
+@app.route('/api/login/confirmacao', methods=['POST'])
+def confirmacao_login():
+    print('\n\n\nConfirmação recebida para login')
+    dados = request.json
+    c = dados['codigo']
+    id = dados['id']
+
+    print('Código de confirmação:', c) # mudar para receber 
+    return confirmacao(c, id)
 
 @app.route('/api/notas', methods=['POST'])
 def scrape_notas():
