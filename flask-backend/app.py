f202de1e606d87a32f9fabc0e8c2f1f7144b0b5e
bruno-a472 <brunoangelo2015@hotmail.com>
2025-06-19 21:01:58 -0300

    feat(sms): Adiciona confirmação sms e altera scrape
    Atualiza sistema para conformidade com o novo site do Siga.

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,7 +1,8 @@
+
 from flask import Flask, request
 from flask_cors import CORS # type: ignore
 
-import uuid
+import time
 import json
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -39,14 +40,16 @@ def login(e: str, s: str, id: int):
         campo_senha = wait.until(EC.presence_of_element_located((By.NAME, "passwd")))
         print('Digitando senha...')
         campo_senha.send_keys(s)
-        botao_entrar = wait.until(EC.element_to_be_clickable((By.ID, "idSIButton9")))
+        # wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "idSIButton9")))
+        try:
+            botao_entrar = wait.until(EC.element_to_be_clickable((By.ID, "idSIButton9")))
+        except TimeoutException:
+            print(botao_entrar, "não carrregou")
+            driver.find_element(By.ID, "idSIButton9").click()
         botao_entrar.click()
 
-        # Espera o botão de confirmação (div do perfil)
+        # Espera o botão de confirmação por SMS
         campo_texto = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "table-row")))
-        nome = campo_texto.find_element(By.TAG_NAME, 'div')
-        # nome = nome.find_element(By.TAG_NAME, 'div').text
-        print(f'Campo encontrado: {nome}')
         campo_texto.click()
 
         print("Aguardando autenticação por dois fatores (digite o código)...")
@@ -55,6 +58,17 @@ def login(e: str, s: str, id: int):
     except TimeoutException as e:
         print("Erro de tempo ao carregar um elemento da página:", str(e))
         return {'bool': False}
+    except Exception as e:
+        print("Erro desconhecido ao carregar um elemento da página:", str(e) + ', tentando novamente.')
+        try:
+            botao_entrar = wait.until(EC.element_to_be_clickable((By.ID, "idSIButton9")))
+            botao_entrar.click()
+            # Espera o botão de confirmação por SMS
+            campo_texto = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "table-row")))
+            campo_texto.click()
+        except Exception as e:
+            print("Erro desconhecido ao carregar um elemento da página:", str(e))
+            return {'bool': False}
     
     resposta = {'bool': True, 'id': id}
     resposta = json.dumps(resposta, ensure_ascii=False, indent=4)
@@ -70,99 +84,121 @@ def confirmacao(c: str, id: int):
     try:
         campo_codigo = wait.until(EC.presence_of_element_located((By.ID, "idTxtBx_SAOTCC_OTC")))
         print('Digitando código...')
+        campo_codigo.clear()
         campo_codigo.send_keys(c)
         campo_codigo.send_keys(Keys.ENTER)
 
         try:
             span = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "uc_appuser-name")))
             nome = 'vazio'
-            nome = str(span.text.strip().lower()).split(' ')[1] 
+            nome = str(span.text.strip().lower()).split(' ')[1].capitalize()
             print(nome)
             if nome != 'vazio':
                 resposta = {'bool': True, 'nome': nome} # Resposta sucesso
-                resposta = json.dumps(resposta, ensure_ascii=False, indent=4)
             else:
                 print("Não foi encontrado o nome:", nome)
         except Exception as e:
             print(e)
+        except TimeoutException:
+            print('Timeout no Login, ID:', id)
+            time.sleep(3)
+            driver.quit()
+            resposta = {'bool': False, 'erro': 'Timeout'}
     except Exception as e:
         print("Um problema foi encontrado:", e)
+        resposta = {'bool': False, 'erro': 'Algum erro ocorreu.'}
+
     print('Terminando requisição confirmação. ID:', id)
+    resposta = json.dumps(resposta, ensure_ascii=False, indent=4)
     return resposta
 
-def notas_parciais(id: int):
+def scrapeNotas(id: int):
+    print("Fazer_scrape")
     driver: webdriver.Chrome = arvore.encontra(id).obtemDriver()
-    # print(f'Pegando notas parciais do driver ID: {arvore.encontra(id).obtemId()}, Driver: {driver}') # Adicionar em caso de testes
-    # Clicar no aba Notas Parciais
+    wait = WebDriverWait(driver, 8)
+    time.sleep(3)
+    print("Fazer_scrape Iniciando")
     try:
-        WebDriverWait(driver, 3).until(
-            EC.presence_of_all_elements_located((By.ID, "ygtvlabelel10Span"))
-        )
-        driver.find_element(By.ID, "ygtvlabelel10Span").click()
-    except:
-        print("Botão não encontrado")
+        # 1 - Clicar em "meu curso"
+        curso_btns = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".uc_appfooter-button.uc_pointer")))
+        for btn in curso_btns:
+            try:
+                print(btn)
+                span = btn.find_elements(By.TAG_NAME, "center")[1]
+                if span.text.strip().lower() == "meu curso":
+                    btn.click()
+                    break
+            except Exception:
+                continue
 
-    # Pegar notas parciais
-    try:
-        WebDriverWait(driver, 3).until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME, "ReadonlyAttribute"))
-        )
+        # 2 - Clicar em "Histórico"
+        historico_btns = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".uc_appgrid-item.uc_pointer")))
+        for btn in historico_btns:
+            try:
+                span = btn.find_element(By.TAG_NAME, "span")
+                if span.text.strip().lower() == "histórico":
+                    btn.click()
+                    break
+            except Exception:
+                continue
+
+        # 3 - Extrair cards
+        wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "uc_appcard")))
+        cards = driver.find_elements(By.CLASS_NAME, "uc_appcard")
+
+        # Coletar os dados
         
-        notas_ele = driver.find_elements(By.XPATH, "//*[starts-with(@id, 'span_vACD_ALUNOHISTORICOITEMMEDIAFINAL_00')]")
-        notas = [nota.text for nota in notas_ele]
-        mates_ele = driver.find_elements(By.XPATH, "//*[starts-with(@id, 'span_vACD_DISCIPLINANOME_00')]")
-        mates = []
-        
-        for i in range(len(mates_ele)):
-            if mates_ele[i].text.startswith("Projeto Integrador"):
-                notas.pop(i)
-            else:
-                mates.append(mates_ele[i].text)
+        mates_p = []
+        notas_p = []
+        status_list_p = []
+        mates_h = []
+        notas_h = []
+        status_list_h = []
 
-        # print("Notas parciais obtidas") # Adicionar em caso de testes
+        for card in cards:
+            try:
+                nome_materia = card.find_element(By.CLASS_NAME, "uc_appcard-title").text.strip()
+                if nome_materia.startswith('Projeto Integrador'):
+                    continue
+                linhas_info = card.find_elements(By.CSS_SELECTOR, ".uc_flex-r.uc_flex-jcsb.uc_w100.uc_mb5")
+                nota = linhas_info[0].find_element(By.CLASS_NAME, "uc_apptext").text.strip()
+                status = linhas_info[4].find_element(By.CLASS_NAME, "uc_apptext").text.strip()
 
-        parcial = [{ "tipo": '', "nome": materia, "nota": nota, 'abc': 'D'} for materia, nota in zip(mates, notas)]
-        return parcial
-    except:
-        print("Os elementos com as notas parciais não foram encontrados.")
-        return 'Falha parciais' # Em caso desse return, pode haver erros. Arrumar depois (1/2)
+                if status == 'Em Curso':
+                    mates_p.append(nome_materia)
+                    notas_p.append(nota)
+                    status_list_p.append(status)
+                else:
+                    mates_h.append(nome_materia)
+                    notas_h.append(nota)
+                    status_list_h.append(status)
 
-def notas_historicas(id: int):
-    # Clicar no aba Notas Histórico
-    driver: webdriver.Chrome = arvore.encontra(id).obtemDriver()
-    try:
-        WebDriverWait(driver, 3).until(
-            EC.presence_of_all_elements_located((By.ID, "ygtvlabelel8Span"))
-        )
-        driver.find_element(By.ID, "ygtvlabelel8Span").click()
-    except:
-        print("Botão Histórico não encontrado")
+            except Exception as e:
+                print(f"Erro ao extrair dados de uma matéria: {e}")
+                continue
+        # Gerar estrutura final
+        parcial = [
+            {"tipo": "a", "nome": materia, "nota": nota, "abc": "D", "status": status}
+            for materia, nota, status in zip(mates_p, notas_p, status_list_p)
+        ]
+        historico = [
+            {"tipo": "h", "nome": materia, "nota": nota, "abc": "D", "status": status}
+            for materia, nota, status in zip(mates_h, notas_h, status_list_h)
+        ]
 
-    # Pegar notas históricas
-    try:
-        WebDriverWait(driver, 3).until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME, "ReadonlyAttribute"))
-        )
-        
-        notas_ele = driver.find_elements(By.XPATH, "//*[starts-with(@id, 'span_vACD_ALUNOHISTORICOITEMMEDIAFINAL_00')]")
-        notas = [nota.text for nota in notas_ele]
-        mates_ele = driver.find_elements(By.XPATH, "//*[starts-with(@id, 'span_vACD_DISCIPLINANOME_00')]")
-        mates = []
-        
-        for i in range(len(mates_ele)):
-            if mates_ele[i].text.startswith("Projeto Integrador"):
-                notas.pop(i)
-            else:
-                mates.append(mates_ele[i].text)
+        resultado = {
+            "parciais": parcial,
+            "historicas": historico
+        }
 
-        # print("Notas históricas obtidas") # Adicionar em caso de testes
-
-        historico = [{ "tipo": 'h', "nome": materia, "nota": nota, 'abc': 'D'} for materia, nota in zip(mates, notas)]
-        
-        return historico
-    except:
-        # print("Os elementos com as notas parciais não foram encontrados.") # Adicionar em caso de testes
-        return 'Falha historicas' # Em caso desse return, pode haver erros. Arrumar depois (2/2)
+        return json.dumps(resultado, ensure_ascii=False, indent=4)
+    
+    finally:
+        print('Feito o scrape de notas, retornando')
+        print(len(resultado['parciais']))
+        print(len(resultado['historicas']))
+        time.sleep(3)
+        driver.quit()
 
 
 app = Flask(__name__)
@@ -212,17 +248,8 @@ def scrape_notas():
     # print(dados)  # Adicionar em caso de testes
     id = dados['id']
 
-    notas_a = notas_parciais(id)
-    notas_h = notas_historicas(id)
-
-    notas = {
-        "parciais": notas_a,
-        "historicas": notas_h
-    }
-
-    notas = json.dumps(notas, ensure_ascii=False, indent=4)
     # print(notas) # Adicionar em caso de testes
-    return notas
+    return scrapeNotas(id)
 
 if __name__ == '__main__':
     app.run(debug=True)
